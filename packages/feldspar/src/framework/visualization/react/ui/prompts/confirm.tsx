fefc296867985f97c4d6b7226470c194c565300a
trbKnl <ncdeschipper@gmail.com>
2026-08-18 11:20:41 +0200

    removed the cancel button from the confirm prompt, this causes participants to be able to continue while they shouldnt

--- packages/feldspar/src/framework/visualization/react/ui/prompts/confirm.tsx
+++ packages/feldspar/src/framework/visualization/react/ui/prompts/confirm.tsx
@@ -26,9 +26,6 @@ export const Confirm = (props: Props): JSX.Element => {
       <BodyLarge text={text} margin='mb-4' />
       <div className='flex flex-row gap-4'>
         <PrimaryButton label={ok} onClick={handleOk} color='text-grey1 bg-tertiary' />
-        {cancel !== undefined && (
-          <PrimaryButton label={cancel} onClick={handleCancel} color='text-white bg-primary' />
-        )}
       </div>
     </>
   )
